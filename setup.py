61ec874c3a31f78eff682f860c6fae41314b6ca8
ZHANG Zhi <850734033@qq.com>
2020-07-30 12:44:13 +0800

    Fix dependence of inplace-abn

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-from torch_tresnet import __version__, __authors__
 import sys
 
 packages = find_packages()
@@ -18,7 +17,7 @@ class PyTest(TestCommand):
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.pytest_args = ""
-
+ 
     def run_tests(self):
         import shlex
         # import here, cause outside the eggs aren't loaded
@@ -29,13 +28,13 @@ class PyTest(TestCommand):
 
 
 setup(name='torch_tresnet',
-      version=__version__,
+      version='1.0.7',
       license='MIT',
       description="TResNet: High Performance GPU-Dedicated Architecture",
       long_description=readme(),
       packages=packages,
       url='https://github.com/tczhangzhi/torch-tresnet',
-      author=__authors__,
+      author='ZHANG Zhi',
       author_email='850734033@qq.com',
       keywords='',
-      install_requires=['torch>=1.3', 'torchvision>=0.4.0', 'inplace-abn'])
+      install_requires=['torch>=1.3', 'torchvision>=0.4.0', 'inplace-abn>=1.0.12'])
